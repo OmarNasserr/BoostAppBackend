99af8a0687a62f39fb11b45e9b8d6a5badef1086
OmarNasser <omarnasserr44@gmail.com>
2024-05-13 17:33:20 +0300

    [UPDT] adding new field to BoostingRequest model

--- BoostApp/boosting_request_app/models.py
+++ BoostApp/boosting_request_app/models.py
@@ -6,12 +6,23 @@ from divisions_app.models import Division
 
 class BoostingRequest(models.Model):
     player_id = models.ForeignKey(BUser, on_delete=models.CASCADE, related_name='boosting_requests')
+    booster_id = models.ForeignKey(BUser, on_delete=models.CASCADE, related_name='applied_to', blank=True, null=True)
+
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
     current_division_id = models.ForeignKey(Division, on_delete=models.CASCADE,
                                             related_name='current_division')
     desired_division_id = models.ForeignKey(Division, on_delete=models.CASCADE,
                                             related_name='desired_division')
     payment_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    is_approved = models.BooleanField(default=False)
+
+    is_confirmed = models.BooleanField(default=True)
+    is_applied = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
+    is_cancelled = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    applied_at = models.DateTimeField(blank=True, null=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+    cancelled_at = models.DateTimeField(blank=True, null=True)
