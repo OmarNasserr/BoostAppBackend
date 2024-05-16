cc59b7135351b0a35ce452be0f6b5bec6221a8b9
OmarNasser <omarnasserr44@gmail.com>
2024-05-16 13:35:11 +0300

    [UPDT] serializer_helper change how datetime fields viewed only in response

--- BoostApp/helper_files/serializer_helper.py
+++ BoostApp/helper_files/serializer_helper.py
@@ -1,20 +1,20 @@
-from rest_framework.exceptions import  ValidationError
+from rest_framework.exceptions import ValidationError
 from collections import OrderedDict
 from rest_framework.relations import PKOnlyObject
 from rest_framework.fields import SkipField
 from django.conf import settings
-
+from rest_framework.fields import DateTimeField
+from datetime import datetime
 from .cryptography import AESCipher
-
 
 aes = AESCipher(settings.SECRET_KEY[:16], 32)
 
+
 class SerializerHelper():
-    
-    
-    #is_valid is overrided to return bool and a error value if exist, this is done to keep a 
-    #consistent response shape {'message':'xxx',...}, therefore the error message is extracted
-    #and returned to the response to be represented in a proper format.
+
+    # is_valid is overrided to return bool and a error value if exist, this is done to keep a
+    # consistent response shape {'message':'xxx',...}, therefore the error message is extracted
+    # and returned to the response to be represented in a proper format.
     def is_valid(self, *, raise_exception=False):
         assert hasattr(self, 'initial_data'), (
             'Cannot call `.is_valid()` as no `data=` keyword argument was '
@@ -32,20 +32,19 @@ class SerializerHelper():
 
         if self._errors and raise_exception:
             raise ValidationError(self.errors)
-        
-        if len(self.errors.keys())!=0:
-            print(self._errors)
-            err=list(self.errors.keys())[0]
-            if self.errors[str(err)][0]=="This field is required.":
-                errReturned="The field '"+str(err)+"' is required"
-            else:
-                errReturned=self.errors[str(err)][0]
-        else:
-            errReturned="no errors were returned"
 
-        return not bool(self._errors),errReturned
-    
-    
+        if len(self.errors.keys()) != 0:
+            print(self._errors)
+            err = list(self.errors.keys())[0]
+            if self.errors[str(err)][0] == "This field is required.":
+                errReturned = "The field '" + str(err) + "' is required"
+            else:
+                errReturned = self.errors[str(err)][0]
+        else:
+            errReturned = "no errors were returned"
+
+        return not bool(self._errors), errReturned
+
     # to_representation function is used to represent data in response, and as we might have 
     # returned encrypted data or data that we would like to be represented as encrypted data 
     # like object_id
@@ -54,17 +53,17 @@ class SerializerHelper():
     # fields_to_be_encrypted is list of fields that aren't encrypted in the database but they
     # are sensitive, therefore we need to encrypt them in the response
     def to_representation(
-        self, instance,
-        fields_to_be_decrypted,
-        fields_to_be_encrypted,
-        fields_to_be_added={}
+            self, instance,
+            fields_to_be_decrypted,
+            fields_to_be_encrypted,
+            fields_to_be_added={}
     ):
         ret = OrderedDict()
         fields = self._readable_fields
-        
+
         for field in fields:
             try:
-                
+
                 attribute = field.get_attribute(instance)
             except SkipField:
                 continue
@@ -81,7 +80,11 @@ class SerializerHelper():
                 elif field.field_name in fields_to_be_encrypted:
                     ret[field.field_name] = aes.encrypt(str(field.to_representation(attribute)))
                 else:
-                    ret[field.field_name] = field.to_representation(attribute)
+                    if isinstance(field, DateTimeField):
+                        ret[field.field_name] = str(attribute.strftime(
+                            "%d %b, %Y - %Ih%Mm%S %p"))
+                    else:
+                        ret[field.field_name] = field.to_representation(attribute)
         if fields_to_be_added is not {}:
             for key in fields_to_be_added.keys():
                 ret[str(key)] = fields_to_be_added[str(key)]
