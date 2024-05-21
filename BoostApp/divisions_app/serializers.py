4ce5600c1da78c96b541d74cb121b9575b5b0fae
OmarNasser <omarnasserr44@gmail.com>
2024-05-21 14:30:09 +0300

    [ADD] new serializer for parent divisions

--- BoostApp/divisions_app/serializers.py
+++ BoostApp/divisions_app/serializers.py
@@ -57,7 +57,6 @@ class DivisionSerializer(serializers.ModelSerializer):
         if 'uploaded_icon' in validated_data:
             uploaded_icon = validated_data.pop('uploaded_icon')
 
-
             division_icon = DivisionImage.objects.filter(division=instance)
             for image in uploaded_icon:
                 division_icon.update(image=image)
@@ -75,5 +74,23 @@ class DivisionSerializer(serializers.ModelSerializer):
         return SerializerHelper.to_representation(
             self=self, instance=instance,
             fields_to_be_decrypted=[],
-            fields_to_be_encrypted=['id','game_id']
+            fields_to_be_encrypted=['id', 'game_id', 'previous_division', 'parent_id']
+        )
+
+
+class ParentDivisionSerializer(serializers.ModelSerializer):
+    division_icon = DivisionIconSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Division
+        exclude = ('created_at', 'updated_at', 'previous_division', 'price', 'rank', 'parent_id')
+
+    def is_valid(self, *, raise_exception=False):
+        return SerializerHelper.is_valid(self=self, raise_exception=raise_exception)
+
+    def to_representation(self, instance):
+        return SerializerHelper.to_representation(
+            self=self, instance=instance,
+            fields_to_be_decrypted=[],
+            fields_to_be_encrypted=['id', 'game_id', 'previous_division', 'parent_id']
         )
