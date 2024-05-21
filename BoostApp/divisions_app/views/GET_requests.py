56a7f67d1f67feedf9952cb5e8f5a19b62003151
OmarNasser <omarnasserr44@gmail.com>
2024-05-21 14:27:46 +0300

    [UPDT] search by gamne_id

--- BoostApp/divisions_app/views/GET_requests.py
+++ BoostApp/divisions_app/views/GET_requests.py
@@ -5,6 +5,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ..serializers import DivisionSerializer
 from ..models import Division
 from ..pagination import DivisionPagination
+from ..validations import DivisionAppValidations
+from helper_files.status_code import Status_code
 
 
 class DivisionsList(generics.ListAPIView):
@@ -14,9 +16,14 @@ class DivisionsList(generics.ListAPIView):
     pagination_class = DivisionPagination
 
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
-    filterset_fields = ['name', ]
-    search_fields = ['name', ]
+    filterset_fields = ['name', 'game_id']
+    search_fields = ['name', 'game_id']
 
     def get(self, request, *args, **kwargs):
+        validation_response = DivisionAppValidations.validate_division_get(self.kwargs)
+        if validation_response.status_code != Status_code.success:
+            return validation_response
+
+        self.kwargs = validation_response.data['kwargs']
         DivisionPagination.set_default_page_number_and_page_size(request)
         return super().get(request, *args, **kwargs)
