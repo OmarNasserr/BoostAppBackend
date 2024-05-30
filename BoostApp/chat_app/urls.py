219a911aaf9484c79ccd8ca5057539f617436c3b
OmarNasser <omarnasserr44@gmail.com>
2024-05-30 17:03:27 +0300

    [UPDT] views urls

--- BoostApp/chat_app/urls.py
+++ BoostApp/chat_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .chat_views.GET_requests import RoomListCreateView, RoomDetailView, MessageListCreateView, MessageDetailView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
+    path('rooms/', RoomListCreateView.as_view(), name='room-list-create'),
+    path('rooms/<int:pk>/', RoomDetailView.as_view(), name='room-detail'),
+    path('messages/', MessageListCreateView.as_view(), name='message-list-create'),
+    path('messages/<int:pk>/', MessageDetailView.as_view(), name='message-detail'),
+
 ]
