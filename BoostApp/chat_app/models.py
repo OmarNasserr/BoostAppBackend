10df2b884368c98e00e6332c99881de1539b3e7d
OmarNasser <omarnasserr44@gmail.com>
2024-05-30 17:02:55 +0300

    [UPDT] chat models

--- BoostApp/chat_app/models.py
+++ BoostApp/chat_app/models.py
@@ -1,3 +1,17 @@
+# models.py
+from user_app.models import BUser
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    booster = models.ForeignKey(BUser, related_name='boosting_rooms', on_delete=models.CASCADE)
+    player = models.ForeignKey(BUser, related_name='playing_rooms', on_delete=models.CASCADE)
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
+    user = models.ForeignKey(BUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('timestamp',)
