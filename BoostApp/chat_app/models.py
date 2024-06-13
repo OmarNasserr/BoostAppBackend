4be36ad44e742e2dd75da3a7f589585386d7f70d
OmarNasser <omarnasserr44@gmail.com>
2024-06-13 16:56:58 +0300

    [ADD] added str function to models

--- BoostApp/chat_app/models.py
+++ BoostApp/chat_app/models.py
@@ -7,11 +7,16 @@ class Room(models.Model):
     booster = models.ForeignKey(BUser, related_name='boosting_rooms', on_delete=models.CASCADE)
     player = models.ForeignKey(BUser, related_name='playing_rooms', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
     user = models.ForeignKey(BUser, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"message in {self.room.name} from user {self.user.username}"
 
     class Meta:
         ordering = ('timestamp',)
