331e39edacccc087c7c6f598dce573b4fdc8d495
OmarNasser <omarnasserr44@gmail.com>
2024-06-13 16:57:45 +0300

    [UPDT] separted http requests from websocket requests

--- BoostApp/BoostApp/urls.py
+++ BoostApp/BoostApp/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path(f"api/v{settings.API_VERSION}/boosting_requests/", include("boosting_request_app.urls")),
     path(f"api/v{settings.API_VERSION}/reviews/", include("reviews_app.urls")),
     path(f"api/v{settings.API_VERSION}/chat/", include("chat_app.urls")),
+    path(f"api/v{settings.API_VERSION}/chat_info/", include("chat_app.http_urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
